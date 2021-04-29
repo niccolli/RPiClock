af66ffff6dc793b5aaae07f0335f8ad402920fc4
Yusuke Umahashi <yusuke@HoneyCat.local>
2021-04-29 16:43:42 +0900

    Client: 実行時の時刻を送信する仕組みを実装

---
+++ RaspberryPi_client/spiclock.py
@@ -0,0 +1,15 @@
+import spidev
+import datetime
+
+spi_bus = 0
+spi_device = 0
+
+spi = spidev.SpiDev()
+spi.open(spi_bus, spi_device)
+spi.max_speed_hz=1000000
+spi.mode= 0b00
+
+dt_now = datetime.datetime.now()
+
+to_send = [dt_now.hour, dt_now.minute, dt_now.second]
+spi.xfer(to_send)
